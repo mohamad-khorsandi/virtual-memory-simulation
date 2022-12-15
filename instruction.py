e26be473707ba720b93f28342a8669ad326f0464
mohammadmolavi <mohammadmolavi97@gmail.com>
2022-12-15 12:50:56 +0330

    instruction class structure

--- instruction.py
+++ instruction.py
@@ -1,6 +1,34 @@
 class Instruction:
     def __int__(self, ins_type, ins_num, process):
-        pass
+        self.ins_type = ins_type
+        self.ins_num = ins_num
+        self.process = process
 
     def run(self):
-        pass
+        if self.process.state == "blocked"
+            return
+        else
+            self.process.state = "running"
+        
+        if self.ins_type == "load":
+            self.process.IR = self.ins_type
+            self.process.TMP = self.ins_num
+            self.process.ACC = self.ins_num
+        elif self.ins_type == "add":
+            self.process.IR = self.ins_type
+            self.process.TMP = self.ins_num
+            self.process.ACC += self.process.TMP
+        elif self.ins_type == "sub":
+            self.process.IR = self.ins_type
+            self.process.TMP = self.ins_num
+            self.process.ACC -= self.process.TMP
+        elif self.ins_type == "mul":
+            self.process.IR = self.ins_type
+            self.process.TMP = self.ins_num
+            self.process.ACC *= self.process.TMP
+        
+        self.process.state = "ready"
+        
+
+
+        
